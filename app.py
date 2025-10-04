025ada95b4dd9aa4e5808267a2a1705c1768e299
Maryam-oth <maahoth@gmail.com>
2025-10-04 19:34:25 +0300

    Add full ap.py

--- app.py
+++ app.py
@@ -166,6 +166,7 @@ def predict():
 # -----------------------------
 # Run App
 # -----------------------------
+
 if __name__ == "__main__":
-    print("Using model:", os.path.abspath('respiratory_classifier.pkl'))
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000
+    app.run(host="0.0.0.0", port=port)
